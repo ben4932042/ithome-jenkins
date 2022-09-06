32aba1ed0b9593327d6103a2688a6b14472031da
Ben.Liu <63529371+ben4932042@users.noreply.github.com>
2022-09-07 03:17:36 +0800

    fix: stage not show error

--- vars/getWebSiteCode.groovy
+++ vars/getWebSiteCode.groovy
@@ -1,26 +1,23 @@
 def call(String domain, Integer code) {
-    return {
-        stage("check website code ${domain}") {
-            script {
-                response_code = sh (
-                    script: "curl -o /dev/null -s -w %{http_code} ${domain}",
-                    returnStdout: true
-                ).toInteger()
+    stage("check website code ${domain}") {
+        script {
+            response_code = sh (
+                script: "curl -o /dev/null -s -w %{http_code} ${domain}",
+                returnStdout: true
+            ).toInteger()
 
-                echo "${response_code}"   
+            echo "${response_code}"   
+        }
+    }
+    stage ("Check response code") {
+        steps{
+            script{
+                if ( response_code != code ){
+                    sh "false"
+                }             
             }
         }
-        stage ("Check response code") {
-            steps{
-                script{
-                    if ( response_code != code ){
-                        sh "false"
-                    }             
-                }
-
-            }
-        }        
-    }
+    }        
 }
 
 
