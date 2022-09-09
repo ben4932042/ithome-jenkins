908f54e4528b62df8f5f6d6162f334a045dd16f1
Ben Liu <ben4932042@icloud.com>
2022-09-09 20:41:05 +0800

    feat: cloud run mode

--- jobs/ithome-day9-sample-k6-testing/ithome-sample.js
+++ jobs/ithome-day9-sample-k6-testing/ithome-sample.js
@@ -2,6 +2,22 @@
 import http from 'k6/http';
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
+export const options = {
+  stages: [
+    { duration: '5s', target: 10 }, 
+    { duration: '1m', target: 10 },
+    { duration: '5s', target: 0 },
+  ],
+  thresholds: {
+    http_req_duration: ['p(99)<1500'],
+  },
+  ext: {
+    loadimpact: {
+      projectID: 3598263,
+      name: "ithome-day9-sample-k6-testing"
+    }
+  }
+};
 
 export default function () {
   http.get('https://test.k6.io');
